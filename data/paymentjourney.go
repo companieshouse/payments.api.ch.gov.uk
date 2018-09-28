91fd5b98414eb25ade5b8056c09821737ef8a2c4
jphillips2121 <jphillips3@companieshouse.gov.uk>
2018-09-28 11:00:44 +0100

    Changed paymentsjourney comments and capitalised NextURL

--- data/paymentjourney.go
+++ data/paymentjourney.go
@@ -1,12 +1,12 @@
 package data
 
-// IncomingPaymentResourceRequest is the data received in the body of the incoming request
+// IncomingExternalPaymentJourneyRequest is the data received in the body of the incoming request
 type IncomingExternalPaymentJourneyRequest struct {
 	PaymentMethod string `json:"payment_method"`
 	Resource      string `json:"resource"`
 }
 
-// PaymentJourney returns the URL required to access external payment provider session
+// ExternalPaymentJourney returns the URL required to access external payment provider session
 type ExternalPaymentJourney struct {
-	NextUrl string `json:"next_url"`
+	NextUrl string `json:"NextURL"`
 }
