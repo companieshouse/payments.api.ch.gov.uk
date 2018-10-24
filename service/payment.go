33442b0cb53f65fba4df6a414c44aa2dd8b4d42b
Dale Bradley <dbradley@companieshouse.gov.uk>
2018-10-24 11:57:01 +0100

    Refactors config to simplify URL generation and limit calls to get config

--- service/payment.go
+++ service/payment.go
@@ -19,7 +19,8 @@ import (
 
 // PaymentService contains the DAO for db access
 type PaymentService struct {
-	DAO dao.DAO
+	DAO    dao.DAO
+	Config config.Config
 }
 
 // CreatePaymentSession creates a payment session and returns a journey URL for the calling app to redirect to
@@ -39,14 +40,7 @@ func (service *PaymentService) CreatePaymentSession(w http.ResponseWriter, req *
 		return
 	}
 
-	cfg, err := config.Get()
-	if err != nil {
-		log.ErrorR(req, fmt.Errorf("error getting config: [%v]", err))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	paymentResource, err := getPaymentResource(w, req, incomingPaymentResourceRequest.Resource, cfg)
+	paymentResource, err := getPaymentResource(w, req, incomingPaymentResourceRequest.Resource, &service.Config)
 	if err != nil {
 		log.ErrorR(req, fmt.Errorf("error getting payment resource: [%v]", err))
 		return
@@ -80,7 +74,7 @@ func (service *PaymentService) CreatePaymentSession(w http.ResponseWriter, req *
 	paymentResource.Reference = incomingPaymentResourceRequest.Reference
 	paymentResource.ID = generateID()
 
-	journeyURL := cfg.PaymentServiceURL + paymentResource.ID + cfg.PaymentServicePath
+	journeyURL := service.Config.PaymentsWebURL + "/payments/" + paymentResource.ID + "/pay"
 	paymentResource.Links = models.Links{
 		Journey: journeyURL,
 	}
