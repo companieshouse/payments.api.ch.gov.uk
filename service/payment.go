0c644853ab7545e05e26d46066ed31c735a07376
jphillips2121 <jphillips3@companieshouse.gov.uk>
2018-11-26 10:35:09 +0000

    Changed patch request to return 403 if resource is not found

--- service/payment.go
+++ service/payment.go
@@ -174,8 +174,14 @@ func (service *PaymentService) PatchPaymentSession(w http.ResponseWriter, req *h
 
 	err = service.DAO.PatchPaymentResource(id, &PaymentResourceUpdate)
 	if err != nil {
+		if err.Error() == "not found" {
+			log.ErrorR(req, fmt.Errorf("could not find payment resource to patch"))
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
 		log.ErrorR(req, fmt.Errorf("error patching payment session on database: [%v]", err))
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	log.InfoR(req, "Successful PATCH request for payment resource", log.Data{"payment_id": id, "status": http.StatusOK})
