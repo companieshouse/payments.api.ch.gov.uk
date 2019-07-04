9dbac2e50ecdd958131579a1cba56749900c7ca3
Michael Acreman <macreman.ch@gmail.com>
2019-07-04 14:21:17 +0100

    parse kafka brokers in start script to split on comma and support multiple brokers

--- start.sh
+++ start.sh
@@ -28,3 +28,11 @@ else
 fi
 
 exec "${APP_DIR}/payments.api.ch.gov.uk" "-bind-addr=:${PORT}"
+
+# Read brokers from environment and split on comma
+IFS=',' read -ra BROKERS <<< "${KAFKA_BROKER_ADDR}"
+
+# Ensure we only populate the broker address via application arguments
+unset KAFKA_BROKER_ADDR
+
+exec "${APP_DIR}/payments.api.ch.gov.uk" "-bind-addr=:${PORT}" $(for broker in "${BROKERS[@]}"; do echo -n "-broker-addr=${broker} "; done)
