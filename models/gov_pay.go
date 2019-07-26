ba9dabd435b7cc93765fb67b678ba9c831564333
divya051988 <dparameswaran@companieshouse.gov.uk>
2019-07-26 15:48:27 +0100

    Missing quotes around TransactionDate json structure

--- models/gov_pay.go
+++ models/gov_pay.go
@@ -103,6 +103,6 @@ type Cancel struct {
 type PaymentDetails struct {
 	CardType        string `json:"card_type"`
 	PaymentID       string `json:"payment_id"`
-	TransactionDate string `json:transaction_date`
+	TransactionDate string `json:"transaction_date"`
 	PaymentStatus   string `json:"payment_status"`
 }
