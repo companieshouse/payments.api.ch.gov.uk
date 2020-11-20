9c33b7f74a781c37faccd3506b4fe2b4b1639191
Jack Phillips <jphillips3@companieshouse.gov.uk>
2020-11-20 10:13:13 +0000

    Revert "Revert "Send email address to GovPay when creating an external resource""

--- service/gov_pay.go
+++ service/gov_pay.go
@@ -60,6 +60,7 @@ func (gp *GovPayService) GenerateNextURLGovPay(req *http.Request, paymentResourc
 	}
 
 	govPayRequest.Amount = amountToPay
+	govPayRequest.Email = paymentResource.CreatedBy.Email
 	govPayRequest.Description = "Companies House Payment" // Hard-coded value for payment screens
 	govPayRequest.Reference = paymentResource.MetaData.ID
 	govPayRequest.ReturnURL = fmt.Sprintf("%s/callback/payments/govpay/%s", gp.PaymentService.Config.PaymentsAPIURL, paymentResource.MetaData.ID)
