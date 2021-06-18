aabdfa44ca26516dbcc1206b36e3fc04bc742abd
Edwin Mead <emead@companieshouse.gov.uk>
2021-06-18 10:10:31 +0100

    Add new privilege interceptor

---
+++ interceptors/payment_privileges.go
@@ -0,0 +1,31 @@
+package interceptors
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/companieshouse/chs.go/authentication"
+	"github.com/companieshouse/chs.go/log"
+)
+
+// ElevatedOrPaymentPrivilegesIntercept checks that the user is authenticated
+func ElevatedOrPaymentPrivilegesIntercept(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check headers for identity type and identity
+		identityType := authentication.GetAuthorisedIdentityType(r)
+		if !(identityType == authentication.APIKeyIdentityType) {
+			log.Error(fmt.Errorf("elevated privileges interceptor unauthorised: not API key identity type"))
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		if authentication.IsKeyElevatedPrivilegesAuthorised(r) || authentication.CheckAuthorisedKeyHasPrivilege(r, "payment") {
+			// Call the next handler
+			next.ServeHTTP(w, r)
+		} else {
+			// If the request is not with an elevated privileges API key then the request is unauthorized
+			w.WriteHeader(http.StatusUnauthorized)
+			log.Error(fmt.Errorf("elevated privileges interceptor unauthorised: not elevated privileges API key"))
+		}
+	})
+}
