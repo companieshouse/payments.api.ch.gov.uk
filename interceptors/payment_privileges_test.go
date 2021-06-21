28075e8420be60f2e51bae8725d3550a30f9b5ed
Edwin Mead <emead@companieshouse.gov.uk>
2021-06-21 16:03:11 +0100

    Add unit test for elevated privilege interceptor

--- interceptors/payment_privileges_test.go
+++ interceptors/payment_privileges_test.go
@@ -1,18 +1,16 @@
 package interceptors
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/companieshouse/chs.go/authentication"
-	"github.com/golang/mock/gomock"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestUnitElevatedOrPaymentPrivilegesIntercept(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
 
 	Convey("Not API Key", t, func() {
 		req, _ := http.NewRequest("GET", "/test", nil)
@@ -51,6 +49,50 @@ func TestUnitElevatedOrPaymentPrivilegesIntercept(t *testing.T) {
 		w := httptest.NewRecorder()
 
 		test := ElevatedOrPaymentPrivilegesIntercept(GetTestHandler())
+		test.ServeHTTP(w, req)
+		So(w.Code, ShouldEqual, http.StatusOK)
+	})
+}
+
+func TestUnitOauth2OrPaymentPrivilegesIntercept(t *testing.T) {
+
+	Convey("Not API Key or OAuth", t, func() {
+		req, _ := http.NewRequest("GET", "/test", nil)
+		w := httptest.NewRecorder()
+
+		test := Oauth2OrPaymentPrivilegesIntercept(GetTestHandler())
+		test.ServeHTTP(w, req)
+		So(w.Code, ShouldEqual, http.StatusUnauthorized)
+	})
+
+	Convey("API Key without payment privilege", t, func() {
+		req, _ := http.NewRequest("GET", "/test", nil)
+		req.Header.Set("ERIC-Identity-Type", authentication.APIKeyIdentityType)
+		w := httptest.NewRecorder()
+
+		test := Oauth2OrPaymentPrivilegesIntercept(GetTestHandler())
+		test.ServeHTTP(w, req)
+		So(w.Code, ShouldEqual, http.StatusUnauthorized)
+		fmt.Println("ENM test end")
+	})
+
+	Convey("API Key with payment privilege", t, func() {
+		req, _ := http.NewRequest("GET", "/test", nil)
+		req.Header.Set("ERIC-Identity-Type", authentication.APIKeyIdentityType)
+		req.Header.Set("ERIC-Authorised-Key-Privileges", "payment")
+		w := httptest.NewRecorder()
+
+		test := Oauth2OrPaymentPrivilegesIntercept(GetTestHandler())
+		test.ServeHTTP(w, req)
+		So(w.Code, ShouldEqual, http.StatusOK)
+	})
+
+	Convey("OAuth2", t, func() {
+		req, _ := http.NewRequest("GET", "/test", nil)
+		req.Header.Set("ERIC-Identity-Type", authentication.Oauth2IdentityType)
+		w := httptest.NewRecorder()
+
+		test := Oauth2OrPaymentPrivilegesIntercept(GetTestHandler())
 		test.ServeHTTP(w, req)
 		So(w.Code, ShouldEqual, http.StatusOK)
 	})
