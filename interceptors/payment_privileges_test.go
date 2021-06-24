274d385c50013fbf2f57e57d9f1b844ba2269b83
Edwin Mead <emead@companieshouse.gov.uk>
2021-06-24 08:10:38 +0100

    Add unit test for UserPaymentAuthenticationIntercept

--- interceptors/payment_privileges_test.go
+++ interceptors/payment_privileges_test.go
@@ -1,7 +1,6 @@
 package interceptors
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -73,7 +72,6 @@ func TestUnitOauth2OrPaymentPrivilegesIntercept(t *testing.T) {
 		test := Oauth2OrPaymentPrivilegesIntercept(GetTestHandler())
 		test.ServeHTTP(w, req)
 		So(w.Code, ShouldEqual, http.StatusUnauthorized)
-		fmt.Println("ENM test end")
 	})
 
 	Convey("API Key with payment privilege", t, func() {
@@ -93,6 +91,86 @@ func TestUnitOauth2OrPaymentPrivilegesIntercept(t *testing.T) {
 		w := httptest.NewRecorder()
 
 		test := Oauth2OrPaymentPrivilegesIntercept(GetTestHandler())
+		test.ServeHTTP(w, req)
+		So(w.Code, ShouldEqual, http.StatusOK)
+	})
+}
+
+func TestUnitUserPaymentAuthenticationIntercept(t *testing.T) {
+
+	Convey("Not API Key or OAuth", t, func() {
+		req, _ := http.NewRequest("GET", "/test", nil)
+		w := httptest.NewRecorder()
+
+		test := UserPaymentAuthenticationIntercept(GetTestHandler())
+		test.ServeHTTP(w, req)
+		So(w.Code, ShouldEqual, http.StatusUnauthorized)
+	})
+
+	Convey("No authorised identity", t, func() {
+		req, _ := http.NewRequest("GET", "/test", nil)
+		req.Header.Set("ERIC-Identity-Type", authentication.APIKeyIdentityType)
+		w := httptest.NewRecorder()
+
+		test := UserPaymentAuthenticationIntercept(GetTestHandler())
+		test.ServeHTTP(w, req)
+		So(w.Code, ShouldEqual, http.StatusUnauthorized)
+	})
+
+	Convey("No authorised user", t, func() {
+		req, _ := http.NewRequest("GET", "/test", nil)
+		req.Header.Set("ERIC-Identity-Type", authentication.Oauth2IdentityType)
+		req.Header.Set("ERIC-Identity", "123")
+		w := httptest.NewRecorder()
+
+		test := UserPaymentAuthenticationIntercept(GetTestHandler())
+		test.ServeHTTP(w, req)
+		So(w.Code, ShouldEqual, http.StatusUnauthorized)
+	})
+
+	Convey("Authorised user length 1", t, func() {
+		req, _ := http.NewRequest("GET", "/test", nil)
+		req.Header.Set("ERIC-Identity-Type", authentication.Oauth2IdentityType)
+		req.Header.Set("ERIC-Identity", "123")
+		req.Header.Set("ERIC-Authorised-User", "test@companieshouse.gov.uk")
+		w := httptest.NewRecorder()
+
+		test := UserPaymentAuthenticationIntercept(GetTestHandler())
+		test.ServeHTTP(w, req)
+		So(w.Code, ShouldEqual, http.StatusOK)
+	})
+
+	Convey("Authorised user length 2", t, func() {
+		req, _ := http.NewRequest("GET", "/test", nil)
+		req.Header.Set("ERIC-Identity-Type", authentication.Oauth2IdentityType)
+		req.Header.Set("ERIC-Identity", "123")
+		req.Header.Set("ERIC-Authorised-User", "test@companieshouse.gov.uk;forename")
+		w := httptest.NewRecorder()
+
+		test := UserPaymentAuthenticationIntercept(GetTestHandler())
+		test.ServeHTTP(w, req)
+		So(w.Code, ShouldEqual, http.StatusOK)
+	})
+
+	Convey("Authorised user length 3", t, func() {
+		req, _ := http.NewRequest("GET", "/test", nil)
+		req.Header.Set("ERIC-Identity-Type", authentication.Oauth2IdentityType)
+		req.Header.Set("ERIC-Identity", "123")
+		req.Header.Set("ERIC-Authorised-User", "test@companieshouse.gov.uk;forename;surname")
+		w := httptest.NewRecorder()
+
+		test := UserPaymentAuthenticationIntercept(GetTestHandler())
+		test.ServeHTTP(w, req)
+		So(w.Code, ShouldEqual, http.StatusOK)
+	})
+
+	Convey("API Key user", t, func() {
+		req, _ := http.NewRequest("GET", "/test", nil)
+		req.Header.Set("ERIC-Identity-Type", authentication.APIKeyIdentityType)
+		req.Header.Set("ERIC-Identity", "123")
+		w := httptest.NewRecorder()
+
+		test := UserPaymentAuthenticationIntercept(GetTestHandler())
 		test.ServeHTTP(w, req)
 		So(w.Code, ShouldEqual, http.StatusOK)
 	})
