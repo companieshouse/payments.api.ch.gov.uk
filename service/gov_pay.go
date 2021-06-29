92013057f3f2e618b6aac94d057d2de8f5135b22
Edwin Mead <emead@companieshouse.gov.uk>
2021-06-29 09:51:12 +0100

    Check if email present

--- service/gov_pay.go
+++ service/gov_pay.go
@@ -61,7 +61,9 @@ func (gp *GovPayService) GenerateNextURLGovPay(req *http.Request, paymentResourc
 	}
 
 	govPayRequest.Amount = amountToPay
-	govPayRequest.Email = paymentResource.CreatedBy.Email
+	if paymentResource.CreatedBy.Email != "" {
+		govPayRequest.Email = paymentResource.CreatedBy.Email
+	}
 	govPayRequest.Description = "Companies House Payment" // Hard-coded value for payment screens
 	govPayRequest.Reference = paymentResource.MetaData.ID
 	govPayRequest.ReturnURL = fmt.Sprintf("%s/callback/payments/govpay/%s", gp.PaymentService.Config.PaymentsAPIURL, paymentResource.MetaData.ID)
