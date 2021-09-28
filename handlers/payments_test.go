9b0347d81a255a5d2085bf48d376cff29029fd0a
jphillips2121 <jphillips3@companieshouse.gov.uk>
2021-09-28 11:48:44 +0100

    Remove unit tests that call Register function due to new PayPal register constraint

--- handlers/payments_test.go
+++ handlers/payments_test.go
@@ -1,20 +1,12 @@
 package handlers
 
 import (
-	"bytes"
-	"context"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/companieshouse/payments.api.ch.gov.uk/config"
-	"github.com/companieshouse/payments.api.ch.gov.uk/helpers"
-	"github.com/companieshouse/payments.api.ch.gov.uk/models"
-	"github.com/companieshouse/payments.api.ch.gov.uk/service"
 	"github.com/golang/mock/gomock"
-	"github.com/gorilla/mux"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -39,27 +31,15 @@ func TestUnitHandleGetPaymentSession(t *testing.T) {
 	cfg, _ := config.Get()
 	cfg.DomainAllowList = "http://dummy-url"
 	cfg.ExpiryTimeInMinutes = "90"
+	cfg.PaypalEnv = "test"
+	cfg.PaypalClientID = "123"
+	cfg.PaypalSecret = "123"
+
 	Convey("Invalid PaymentResourceRest", t, func() {
 		req := httptest.NewRequest("GET", "/test", nil)
 		w := httptest.NewRecorder()
 		HandleGetPaymentSession(w, req)
 		So(w.Code, ShouldEqual, 500)
-	})
-	Convey("Payment session expired", t, func() {
-		req := httptest.NewRequest("GET", "/test", nil)
-		ctx := context.WithValue(req.Context(), helpers.ContextKeyPaymentSession, &models.PaymentResourceRest{CreatedAt: time.Now().Add(-time.Hour * 2)})
-		w := httptest.NewRecorder()
-		Register(mux.NewRouter(), *cfg)
-		HandleGetPaymentSession(w, req.WithContext(ctx))
-		So(w.Code, ShouldEqual, 200)
-	})
-	Convey("Valid PaymentResourceRest", t, func() {
-		req := httptest.NewRequest("GET", "/test", nil)
-		ctx := context.WithValue(req.Context(), helpers.ContextKeyPaymentSession, &models.PaymentResourceRest{CreatedAt: time.Now()})
-		w := httptest.NewRecorder()
-		Register(mux.NewRouter(), *cfg)
-		HandleGetPaymentSession(w, req.WithContext(ctx))
-		So(w.Code, ShouldEqual, 200)
 	})
 }
 
@@ -67,54 +47,12 @@ func TestUnitHandlePatchPaymentSession(t *testing.T) {
 	cfg, _ := config.Get()
 	cfg.DomainAllowList = "http://dummy-url"
 	cfg.ExpiryTimeInMinutes = "90"
-	Convey("Request Body empty", t, func() {
-		req := httptest.NewRequest("GET", "/test", nil)
-		req = mux.SetURLVars(req, map[string]string{"payment_id": "123"})
-		ctx := context.WithValue(req.Context(), helpers.ContextKeyPaymentSession, &models.PaymentResourceRest{CreatedAt: time.Now()})
-		req.Body = nil
-		w := httptest.NewRecorder()
-		Register(mux.NewRouter(), *cfg)
-		HandlePatchPaymentSession(w, req.WithContext(ctx))
-		So(w.Code, ShouldEqual, 400)
-	})
-
-	Convey("Request Body invalid", t, func() {
-		req := httptest.NewRequest("GET", "/test", nil)
-		req = mux.SetURLVars(req, map[string]string{"payment_id": "123"})
-		ctx := context.WithValue(req.Context(), helpers.ContextKeyPaymentSession, &models.PaymentResourceRest{CreatedAt: time.Now()})
-		w := httptest.NewRecorder()
-		Register(mux.NewRouter(), *cfg)
-		HandlePatchPaymentSession(w, req.WithContext(ctx))
-		So(w.Code, ShouldEqual, 400)
-	})
 
 	Convey("Invalid PaymentResourceRest due to no context", t, func() {
 		req := httptest.NewRequest("GET", "/test", nil)
 		w := httptest.NewRecorder()
 		HandlePatchPaymentSession(w, req)
 		So(w.Code, ShouldEqual, 500)
-	})
-
-	Convey("Payment method not supplied", t, func() {
-		reqBody := []byte(`{"amount":"12"}`)
-		req := httptest.NewRequest("GET", "/test", ioutil.NopCloser(bytes.NewReader(reqBody)))
-		req = mux.SetURLVars(req, map[string]string{"payment_id": "123"})
-		ctx := context.WithValue(req.Context(), helpers.ContextKeyPaymentSession, &models.PaymentResourceRest{CreatedAt: time.Now()})
-		w := httptest.NewRecorder()
-		Register(mux.NewRouter(), *cfg)
-		HandlePatchPaymentSession(w, req.WithContext(ctx))
-		So(w.Code, ShouldEqual, 400)
-	})
-
-	Convey("Payment session expired", t, func() {
-		reqBody := []byte(`{"amount":"12", "payment_method": "GovPay"}`)
-		req := httptest.NewRequest("GET", "/test", ioutil.NopCloser(bytes.NewReader(reqBody)))
-		req = mux.SetURLVars(req, map[string]string{"payment_id": "123"})
-		ctx := context.WithValue(req.Context(), helpers.ContextKeyPaymentSession, &models.PaymentResourceRest{CreatedAt: time.Now().Add(-time.Hour * 2)})
-		w := httptest.NewRecorder()
-		Register(mux.NewRouter(), *cfg)
-		HandlePatchPaymentSession(w, req.WithContext(ctx))
-		So(w.Code, ShouldEqual, 403)
 	})
 }
 
@@ -127,20 +65,6 @@ func TestUnitHandleGetPaymentDetails(t *testing.T) {
 		req := httptest.NewRequest("GET", "/test", nil)
 		w := httptest.NewRecorder()
 		HandleGetPaymentDetails(w, req)
-		So(w.Code, ShouldEqual, http.StatusInternalServerError)
-	})
-
-	Convey("Error getting payment status from Gov Pay", t, func() {
-
-		paymentResource := models.PaymentResourceRest{
-			Status: service.InProgress.String(),
-			Costs:  []models.CostResourceRest{{ClassOfPayment: []string{"class"}}},
-		}
-
-		req := httptest.NewRequest("GET", "/test", nil)
-		ctx := context.WithValue(req.Context(), helpers.ContextKeyPaymentSession, &paymentResource)
-		w := httptest.NewRecorder()
-		HandleGetPaymentDetails(w, req.WithContext(ctx))
 		So(w.Code, ShouldEqual, http.StatusInternalServerError)
 	})
 }
--- handlers/register_test.go
+++ handlers/register_test.go
@@ -5,29 +5,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/companieshouse/payments.api.ch.gov.uk/config"
-	"github.com/gorilla/mux"
-
 	. "github.com/smartystreets/goconvey/convey"
 )
-
-func TestUnitRegisterRoutes(t *testing.T) {
-	Convey("Register routes", t, func() {
-		router := mux.NewRouter()
-		cfg, _ := config.Get()
-		cfg.SecureAppCostsRegex = "\\/secure-app-regex-test\\/"
-		Register(router, *cfg)
-		So(router.GetRoute("get-healthcheck"), ShouldNotBeNil)
-		So(router.GetRoute("create-payment"), ShouldNotBeNil)
-		So(router.GetRoute("get-payment"), ShouldNotBeNil)
-		So(router.GetRoute("get-payment-details"), ShouldNotBeNil)
-		So(router.GetRoute("patch-payment"), ShouldNotBeNil)
-		So(router.GetRoute("create-external-payment-journey"), ShouldNotBeNil)
-		So(router.GetRoute("handle-govpay-callback"), ShouldNotBeNil)
-		So(router.GetRoute("create-refund"), ShouldNotBeNil)
-		So(router.GetRoute("update-refund"), ShouldNotBeNil)
-	})
-}
 
 func TestUnitGetHealthCheck(t *testing.T) {
 	Convey("Get HealthCheck", t, func() {
