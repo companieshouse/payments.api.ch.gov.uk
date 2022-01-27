1af6960ad5621976de05de197962a6c8058af513
Joseph-Nash <jnash@companieshouse.gov.uk>
2022-01-27 15:45:56 +0000

    WIP - add admin role interceptor

--- helpers/authentication_helper.go
+++ helpers/authentication_helper.go
@@ -2,3 +2,6 @@ package helpers
 
 // AdminPaymentLookupRole defines the path to check whether a user is authorised to look up a payment.
 const AdminPaymentLookupRole = "/admin/payment-lookup"
+
+// AdminPenaltyLookupRole defines the path to check whether a user is authorised to refund bulk payments.
+const AdminBulkRefundRole = "/admin/payments/bulk-refunds"
