b00afc0194172b0ee83d2e7e783008c09060d9c1
Rohan Sachdev <rsachdev@companieshouse.gov.uk>
2022-02-24 16:32:32 +0000

    Add context key helper for userID

--- helpers/context_key.go
+++ helpers/context_key.go
@@ -5,3 +5,6 @@ type ContextKey string
 
 // ContextKeyPaymentSession is a specific key for identifying "payment_session" contexts added to the http request
 var ContextKeyPaymentSession = ContextKey("payment_session")
+
+// ContextKeyUserID is a specific key for identifying "user_id" contexts added to the http request
+var ContextKeyUserID = ContextKey("user_id")
