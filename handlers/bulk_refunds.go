f64677e5064c26da73f640bfa2a1072dedba4ceb
Edwin Mead <emead@companieshouse.gov.uk>
2022-04-28 14:35:08 +0100

    Add validation for PayPal refund file BI-11016

--- handlers/bulk_refunds.go
+++ handlers/bulk_refunds.go
@@ -78,23 +78,15 @@ func handleRefundFile(w http.ResponseWriter, req *http.Request, paymentProvider 
 		return
 	}
 
+	batchRefund.PaymentProvider = paymentProvider
+
 	var validationErrors []string
 
 	// Validate batch refund request data against data in DB
-	switch paymentProvider {
-	case paypalProvider:
-	case govpayProvider:
-		validationErrors, err = refundService.ValidateGovPayBatchRefund(req.Context(), batchRefund)
-		if err != nil {
-			log.ErrorR(req, err)
-			m := utils.NewMessageResponse("error processing batch refund")
-			utils.WriteJSONWithStatus(w, req, m, http.StatusInternalServerError)
-			return
-		}
-	default:
-		message := fmt.Sprintf("invalid payment provider: %s", paymentProvider)
-		log.Debug(message)
-		m := utils.NewMessageResponse(message)
+	validationErrors, err = refundService.ValidateBatchRefund(req.Context(), batchRefund)
+	if err != nil {
+		log.ErrorR(req, err)
+		m := utils.NewMessageResponse("error processing batch refund")
 		utils.WriteJSONWithStatus(w, req, m, http.StatusInternalServerError)
 		return
 	}
