27fcf078cf4db5843df3120dc1158bf89869d314
jphillipsch <jphillips5@companieshouse.gov.uk>
2025-06-18 11:45:31 +0100

    fix docker start when connecting to multiple brokers

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -3,4 +3,10 @@
 # Start script for payments-api
 PORT="20188"
 
-exec ./payments.api.ch.gov.uk "-bind-addr=:${PORT}"
+# Read brokers and topics from environment and split on comma
+IFS=',' read -ra BROKERS <<< "${KAFKA_BROKER_ADDR}"
+
+# Ensure we only populate the broker address and topic via application arguments
+unset KAFKA_BROKER_ADDR
+
+exec ./payments.api.ch.gov.uk "-bind-addr=:${PORT}" $(for broker in "${BROKERS[@]}"; do echo -n "-broker-addr=${broker} "; done)
